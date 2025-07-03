e02fea881c6bbb3df5a7f7b625cdcbd3920c7404
zhujl <792484931@qq.com>
2025-07-03 14:42:24 +0800

    fix jump again

--- scripts/dir-to-html.js
+++ scripts/dir-to-html.js
@@ -2,14 +2,22 @@ const fs = require('fs');
 const path = require('path');
 
 const outDir = path.join(__dirname, '../out');
-const dirs = fs.readdirSync(outDir, { withFileTypes: true });
 
-dirs.forEach(dirent => {
-  if (dirent.isDirectory()) {
-    const indexPath = path.join(outDir, dirent.name, 'index.html');
-    const htmlPath = path.join(outDir, `${dirent.name}.html`);
-    if (fs.existsSync(indexPath) && !fs.existsSync(htmlPath)) {
-      fs.copyFileSync(indexPath, htmlPath);
+function walk(dir) {
+  fs.readdirSync(dir, { withFileTypes: true }).forEach(dirent => {
+    const fullPath = path.join(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      const indexPath = path.join(fullPath, 'index.html');
+      const rel = path.relative(outDir, fullPath);
+      if (rel) {
+        const htmlPath = path.join(outDir, `${rel}.html`);
+        if (fs.existsSync(indexPath) && !fs.existsSync(htmlPath)) {
+          fs.copyFileSync(indexPath, htmlPath);
+        }
+      }
+      walk(fullPath); // 递归子目录
     }
-  }
-}); 
+  });
+}
+
+walk(outDir); 
